1fd3aeb2bff1790824ec23b1a56edf196b4f8fa1
ken_aikawa <xliangu@gmail.com>
2023-04-17 15:58:06 +0900

    commit message

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :blogs
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+end
